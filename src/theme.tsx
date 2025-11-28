190e65931eb150824c25870ff4057caabb072011
Aleksi Salmela <aleksi.salmela@giosg.com>
2025-11-28 20:02:40 +0200

    Fix the types for the src/theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,12 +1,14 @@
-import { useContext, createContext, Component } from 'solid-js';
+import { useContext, createContext, Component, JSX } from 'solid-js';
 
 const ThemeContext = createContext(() => ({}));
 
-export function useTheme(): any {
+export type Theme = Record<string, any>;
+
+export function useTheme(): Theme {
   return useContext(ThemeContext)();
 }
 
-export const ThemeProvider: Component<{theme: () => any}> = (props) => {
+export const ThemeProvider: Component<{theme: () => Theme; children?: JSX.Element}> = (props) => {
   return (
     <ThemeContext.Provider value={props.theme}>
       {props.children}
